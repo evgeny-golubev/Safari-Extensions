e17a80c98accece084840cf12eaaedb2951ff01a
Evgeny Golubev <evgeny@golubev.eu>
2013-02-07 11:49:45 +0300

    no message

--- Harvest4Basecamp/Harvest4Basecamp.safariextension/js/popup.js
+++ Harvest4Basecamp/Harvest4Basecamp.safariextension/js/popup.js
@@ -6,7 +6,7 @@ function H4BPopup(){
 		var basecamp_id = safari.extension.secureSettings.basecamp_id;
 		
 		var initPopup = function(){
-			if(harvest_subdomain != '' && harvest_email != '' && harvest_password != '' && basecamp_id != ''){
+			if(harvest_subdomain != '' && basecamp_id != ''){
 				container.append('<div class="services"></div>');
 				container.find('.services').append('<p><a href="https://'+harvest_subdomain+'.harvestapp.com/time"><img src="images/Harvest.png" alt="Harvest App" /></a></p>');
 				container.find('.services').append('<p><a href="https://basecamp.com/'+basecamp_id+'"><img src="images/Basecamp.png" alt="Basecamp App" /></a></p>');
