b6a60881acd1ee81cf36eac15151b0262f165577
Evgeny Golubev <evgeny@golubev.eu>
2014-05-10 14:53:00 +0300

    Fix H4B

--- Harvest4Basecamp/Harvest4Basecamp.safariextension/js/basecamp.js
+++ Harvest4Basecamp/Harvest4Basecamp.safariextension/js/basecamp.js
@@ -1,202 +1,123 @@
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-function H4BMain(){
+  (function() {
+    var BasecampProfile;
+    BasecampProfile = (function() {
+      function BasecampProfile() {
+        this.addTimer = __bind(this.addTimer, this);
+        this.addTimers = __bind(this.addTimers, this);
+        this.projectNameSelector = "h1";
+        this.itemSelector = ".todo .content";
+        this.platformLoaded = false;
+        this.interval = 250;
+        this.loadHarvestPlatform();
+        window.setInterval(this.addTimers, this.interval);
+      }
 
-	$(document).ready(function(){
-        var projectNameSelector = 'title:first';
-
-        var itemSelector = '.todo .content';
-        var timersSelector = '.harvest-timer:not(.styled)';
-
-        var isConfigLoaded = false;
-
-        var config = "window._harvestPlatformConfig = {'applicationName': 'Basecamp', 'permalink': 'https://basecamp.com/%ACCOUNT_ID%/projects/%PROJECT_ID%/todos/%ITEM_ID%', 'skipJquery': true}";
-
-        var namespacedString = function(name) {
-            return "harvest-" + name;
+      BasecampProfile.prototype.loadHarvestPlatform = function() {
+        var configScript, ph, platformConfig, platformScript,
+          _this = this;
+        platformConfig = {
+          applicationName: "Basecamp",
+          permalink: "https://basecamp.com/%ACCOUNT_ID%/projects/%PROJECT_ID%/todos/%ITEM_ID%",
+          environment: "production"
         };
+        configScript = document.createElement("script");
+        configScript.innerHTML = "window._harvestPlatformConfig = " + (JSON.stringify(platformConfig)) + ";";
+        platformScript = document.createElement("script");
+        platformScript.src = "//platform.harvestapp.com/assets/platform.js";
+        platformScript.async = true;
+        document.body.insertBefore(configScript, document.body.firstChild);
+        document.body.insertBefore(platformScript, document.body.firstChild);
+        return document.body.addEventListener("harvest-event:ready", function() {
+          _this.platformLoaded = true;
+          return _this.addTimers();
+        });
+      };
 
-        var nsId = function(name) {
-            return "#" + (namespacedString(name));
-        };
-
-        var nsEvent = function(name) {
-            return "" + (namespacedString('event')) + ":" + name;
-        };
-
-        var insertScript = function(callback) {
-            var entry, script;
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.async = true;
-            script.innerHTML = config;
-            entry = document.getElementsByTagName('script')[0];
-            entry.parentNode.insertBefore(script, entry);
-            if (script.addEventListener) {
-                script.addEventListener('load', callback, false);
-            } else {
-                script.attachEvent('onreadystatechange', function() {
-                    if (/complete|loaded/.test(script.readyState)) {
-                        return callback();
-                    }
-                });
-            }
-            return isConfigLoaded = true;
-        };
-
-        var loadConfig = function() {
-            var url = "//platform.harvestapp.com/assets/platform.js";
-            var callback = $.getScript(url);
-            return insertScript(callback);
-        };
-
-        var findAccountId = function(item) {
-            var href, link, pattern;
-            pattern = /^\/(\d+)\/projects\/\S+\/todos\/\d+\S+$/;
-            link = $(item).find('a');
-            href = link.attr("href");
-            if (href) {
-                return href.match(pattern)[1];
-            }
-        };
-
-        var findProjectId = function(item) {
-            var href, link, pattern;
-            pattern = /^\/\d+\/projects\/(\d+)\S+\/todos\/\d+\S+$/;
-            item = $(item);
-            link = item.find('a');
-            href = link.attr('href');
-            if (href) {
-                return href.match(pattern)[1];
-            }
-        };
-
-        var findItemId = function(item) {
-            var href, link, pattern;
-            pattern = /^\/\d+\/projects\/\d+\S+\/todos\/(\d+)\S+$/;
-            item = $(item);
-            link = item.find('a');
-            href = link.attr('href');
-            if (href) {
-                return href.match(pattern)[1];
-            }
-        };
-
-        var findProjectName = function() {
-            return $(projectNameSelector).text();
-        };
-
-        var findDescription = function(item) {
-            return item.find('a').text();
-        };
-
-        var findIdsForItem = function(item) {
-            var data;
-            if (!(item instanceof jQuery)) {
-                item = $(item);
-            }
-            return data = {
-                account: {
-                    id: findAccountId(item)
-                },
-                project: {
-                    id: findProjectId(item),
-                    name: findProjectName()
-                },
-                item: {
-                    id: findItemId(item),
-                    name: findDescription(item)
-                }
-            };
-        };
-
-        var addButtons = function() {
-        	var newTimers = false;
-
-            $(itemSelector).each(function() {
-                if (!isButtonAdded(this)) {
-                    var data = findIdsForItem(this);
-                    if (!isItemComplete(this) && isDataSufficient(data)) {
-                    	newTimers = true;
-                    	addButton(this, data);
-                    }
-                }
-            });
-
-            if(newTimers)
-            {
-			    div = $("" + (nsId('messaging'))).get(0);
-			    if (div) {
-			      return $(div).trigger({type: nsEvent('timers:add')});
-			    }
-            }
-        };
-
-        var addButton = function(item, data) {
-            if (!(item instanceof jQuery)) {
-                item = $(item);
-            }
-            var timer = $('<div class="harvest-timer"></div>')
-                        .attr('data-account', JSON.stringify(data.account))
-                        .attr('data-project', JSON.stringify(data.project))
-                        .attr('data-item', JSON.stringify(data.item));
-            var element = $(item).before(timer);
-        };
-
-        var isItemComplete = function(item) {
-            if (!(item instanceof jQuery)) {
-                item = $(item);
-            }
-            return item.closest(".complete").length === 1;
-        };
-
-        var isButtonAdded = function(item) {
-            if (!(item instanceof jQuery)) {
-                item = $(item);
-            }
-            return item.prev().hasClass('harvest-timer');
-        };
-
-        var isDataSufficient = function(data) {
-            var _ref, _ref1;
-            if (!(data != null)) {
-                return false;
-            }
-            return (((_ref = data.project) != null ? _ref.id : void 0) != null) && (((_ref1 = data.item) != null ? _ref1.id : void 0) != null);
-        };
-
-        loadConfig();
-
-        setInterval(function() {
-            var href, pattern;
-            pattern = /^https:\/\/basecamp.com\/\d+\/.*$/;
-            href = window.location.href;
-            if (href.match(pattern) !== null) {
-                if (!isConfigLoaded) {
-                    loadConfig();
-                } else {
-                    addButtons();
-                }
-            }
-        }, 250);
-    });
-
-    JSON.stringify = JSON.stringify || function (obj) {
-        var t = typeof (obj);
-        if (t != "object" || obj === null) {
-            if (t == "string") obj = '"'+obj+'"';
-            return String(obj);
+      BasecampProfile.prototype.addTimers = function() {
+        var item, items, _i, _len, _results;
+        if (!this.platformLoaded) {
+          return;
         }
-        else {
-            var n, v, json = [], arr = (obj && obj.constructor == Array);
-            for (n in obj) {
-                v = obj[n]; t = typeof(v);
-                if (t == "string") v = '"'+v+'"';
-                else if (t == "object" && v !== null) v = JSON.stringify(v);
-                json.push((arr ? "" : '"' + n + '":') + String(v));
-            }
-            return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+        items = document.querySelectorAll(this.itemSelector);
+        _results = [];
+        for (_i = 0, _len = items.length; _i < _len; _i++) {
+          item = items[_i];
+          if (!item.querySelector(".harvest-timer")) {
+            _results.push(this.addTimer(item));
+          }
         }
-    };
-}
+        return _results;
+      };
 
-H4BMain();
+      BasecampProfile.prototype.addTimer = function(item) {
+        var data;
+        data = this.getDataForTimer(item);
+        if (this.isTodoCompleted(item) || this.notEnoughInfo(data)) {
+          return;
+        }
+        this.buildTimer(item, data);
+        return this.notifyPlatformOfNewTimers();
+      };
+
+      BasecampProfile.prototype.getDataForTimer = function(item) {
+        var itemName, link, linkParts, projectName;
+        itemName = (item.querySelector("a[title]") || item.querySelector("a")).innerText;
+        projectName = document.querySelector(this.projectNameSelector).innerText;
+        link = item.querySelector("a").getAttribute("href") || "";
+        linkParts = link.match(/^\/(\d+)\/projects\/(\d+)(?:\S+)?\/todos\/(\d+)/);
+        return {
+          account: {
+            id: linkParts[1]
+          },
+          project: {
+            id: linkParts[2],
+            name: projectName
+          },
+          item: {
+            id: linkParts[3],
+            name: itemName
+          }
+        };
+      };
+
+      BasecampProfile.prototype.isTodoCompleted = function(item) {
+        if (item.webkitMatchesSelector(".complete")) {
+          return true;
+        } else if (item.parentNode && item.parentNode !== document) {
+          return this.isTodoCompleted(item.parentNode);
+        }
+      };
+
+      BasecampProfile.prototype.notEnoughInfo = function(data) {
+        var _ref, _ref1;
+        return !(((data != null ? (_ref = data.project) != null ? _ref.id : void 0 : void 0) != null) && ((data != null ? (_ref1 = data.item) != null ? _ref1.id : void 0 : void 0) != null));
+      };
+
+      BasecampProfile.prototype.buildTimer = function(item, data) {
+        var timer;
+        timer = document.createElement("div");
+        timer.className = "harvest-timer";
+        timer.style.marginRight = "4px";
+        timer.setAttribute("id", "harvest-basecamp-timer-" + data.item.id);
+        timer.setAttribute("data-account", JSON.stringify(data.account));
+        timer.setAttribute("data-project", JSON.stringify(data.project));
+        timer.setAttribute("data-item", JSON.stringify(data.item));
+        return item.insertBefore(timer, item.children[0]);
+      };
+
+      BasecampProfile.prototype.notifyPlatformOfNewTimers = function() {
+        var evt;
+        evt = new CustomEvent("harvest-event:timers:chrome:add");
+        return document.querySelector("#harvest-messaging").dispatchEvent(evt);
+      };
+
+      return BasecampProfile;
+    })();
+
+    return new BasecampProfile();
+  })();
+
+}).call(this);
